8b6f1191772444648cd495658d95dacd383d87a1
Serge Boyko <you@example.com>
2019-04-06 14:08:28 -0400

    Pagination test done

---
+++ src/components/UI/Pagination/Pagination.test.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import Pagination from './Pagination'
+import { shallow } from 'enzyme'
+import { findByTestAtrr, checkProps } from "../../utiles/testFunctions"
+import { debug } from 'util';
+
+const setUp = (props = {}) => {
+    const component = shallow(<Pagination {...props} />)
+    return component
+}
+
+// describe('Checking PropTypes', () => {
+
+//     it('Should NOT throw a warning', () => {
+//         const expectedProps = {
+//             moviesCount: 1,
+//             pageSize: 1,
+//             currentPage: 1,
+//             onPageChange: () => { }
+//         };
+//         const propsError = checkProps(Pagination, expectedProps);
+//         expect(propsError).toBeUndefined();
+//     });
+
+// });
+
+describe('Should render <Pagination />', () => {
+
+    describe('Should render pagination if pageCount > 1', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            const props = {
+                moviesCount: 10,
+                pageSize: 2,
+                currentPage: 1
+
+            }
+            wrapper = setUp(props)
+        })
+
+        it(' pageCount > 1 ', () => {
+            const component = findByTestAtrr(wrapper, 'PaginationComponent')
+            //  console.log(component.debug());
+            expect(component).toHaveLength(1);
+        })
+    })
+
+    describe('Should NOT render pagination if pageCount === 1', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            const props = {
+                moviesCount: 1,
+                pageSize: 1,
+                currentPage: 1,
+                onPageChange: () => { }
+
+            }
+            wrapper = setUp(props)
+        })
+
+        it(' pageCount === 1 ', () => {
+            const component = findByTestAtrr(wrapper, 'PaginationComponent')
+            //   console.log(component.debug());
+            expect(component).toBeDefined()
+        })
+    })
+
+    describe('Should render paragraph No Movie if pageCount === 0', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            const props = {
+                moviesCount: 0,
+                pageSize: 1,
+                currentPage: 1,
+                onPageChange: () => { }
+
+            }
+            wrapper = setUp(props)
+        })
+
+        it(' pageCount === 0 ', () => {
+            const component = findByTestAtrr(wrapper, 'NoMovies')
+            console.log(component.debug());
+            expect(component).toHaveLength(1);
+        })
+    })
+
+
+})
